794bffed715ff2e8f27ee097e496a1b26db53168
hemengke1997 <49073282+hemengke1997@users.noreply.github.com>
2023-10-16 06:32:56 +0000

    Deploying to gh-pages from  @ 636b7a649127a15c67244a832f4d814d33c16555 🚀

---
+++ out/haha.5ff2aef3.js
@@ -0,0 +1,1 @@
+(()=>{console.log("custom define!","\u6765\u81EAvite define\u7684\u73AF\u5883\u53D8\u91CF");})();
---
+++ out/test.09b479d0.js
@@ -0,0 +1,1 @@
+(()=>{console.log("test","\u7B80\u5355\u6253\u5370\u4E00\u4E2A test \u5B57\u7B26\u4E32");})();
